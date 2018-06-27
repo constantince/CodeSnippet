95a4d5f1d4ec4bbfd1df33d070f89d94e09a3f0c
constantince <alberteinstein007@126.com>
2018-06-27 17:21:33 +0800

    add web component TouchFeedBack

---
+++ React/TouchFeedBack.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './index.css';
+export default class TouchFeedBack extends Component {
+    static propTypes = {
+        className: PropTypes.string,
+        children: PropTypes.element,
+        onClick: PropTypes.func
+    };
+
+    state = {
+      currentStyle: ''
+    };
+
+    touchStart = () => {
+        this.setState({
+            currentStyle: {
+                opacity: .5,
+                backgroundColor: 'transparent',
+               '-webkit-transform': 'translate(.03rem, .03rem)',
+               '-webkit-tap-highlight-color' : 'transparent',
+               transform: 'translate(.03rem, .03rem)',
+            }
+        });
+    };
+
+    touchEnd = () => {
+        this.setState({
+            currentStyle: {}
+        });
+    };
+
+    render() {
+        const {className, children, style, onClick} = this.props;
+        return(<div className={className + ' link-feedback '}
+                    style={{...this.state.currentStyle, ...style}}
+                    onTouchStart={this.touchStart}
+                    onTouchEnd={this.touchEnd}
+                    onClick={onClick}
+            >
+                {children}
+            </div>
+        )
+    }
+}
